a9136bc7ae075f40a695ec3100fb27afda97cc41
camiiprd <camila.prado.031@gmail.com>
2025-01-31 16:09:53 -0300

    crud turnos funcionando

---
+++ Back/src/routes/GestionTurnos/turnos.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createTurno, deleteTurno, getTurnos, updateTurno } from "../../controllers/GestionTurnos/turnos.js";
+
+
+const router = Router();
+
+router.get("/", getTurnos);
+router.post("/", createTurno);
+router.put("/:id", updateTurno);
+router.delete("/:id", deleteTurno);
+
+export default router;
