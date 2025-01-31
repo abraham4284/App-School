f6637c69c4216700c2643bedabeba7e13d5d8178
camiiprd <camila.prado.031@gmail.com>
2025-01-31 16:10:06 -0300

    crud tutores funciona

---
+++ Back/src/routes/GestionTutor/tutor.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createTutor, deleteTutor, getTutores, updateTutor } from "../../controllers/GestionTutor/tutor.js";
+
+
+const router = Router();
+
+router.get("/", getTutores);  // Obtener todos los tutores
+router.post("/", createTutor);  // Crear un nuevo tutor
+router.put("/:id", updateTutor);  // Actualizar un tutor existente
+router.delete("/:id", deleteTutor);  // Eliminar un tutor
+
+export default router;
